865fdf28363e7d0945a22add90e809d72e671ae8
Doh Halle <u_mhallen@hotmail.com>
2021-11-16 11:42:53 +0100

    Initial Commit

---
+++ cmd/web/main.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/doh-halle/ntarikoon-park/pkg/config"
+	"github.com/doh-halle/ntarikoon-park/pkg/handlers"
+	"github.com/doh-halle/ntarikoon-park/pkg/render"
+
+	"github.com/alexedwards/scs/v2"
+)
+
+const portNumber = ":9000"
+
+var app config.AppConfig
+var session *scs.SessionManager
+
+//Main is the main application function
+func main() {
+
+	//Change this to true when in production
+	app.InProduction = false
+
+	session = scs.New()
+	session.Lifetime = 24 * time.Hour
+	session.Cookie.Persist = true
+	session.Cookie.SameSite = http.SameSiteLaxMode
+	session.Cookie.Secure = app.InProduction
+
+	app.Session = session
+
+	tc, err := render.CreateTemplateCache()
+	if err != nil {
+		log.Fatal("Cannot create template Cache")
+	}
+
+	app.TemplateCache = tc
+	app.UseCache = false
+
+	repo := handlers.NewRepo(&app)
+	handlers.NewHandlers(repo)
+
+	render.NewTemplates(&app)
+
+	//http.HandleFunc("/", handlers.Repo.Home)
+	//http.HandleFunc("/about", handlers.Repo.About)
+
+	fmt.Println(fmt.Printf("Starting Go Mulang Web Application on port %s", portNumber))
+	//_ = http.ListenAndServe(portNumber, nil)
+
+	srv := &http.Server{
+		Addr:    portNumber,
+		Handler: routes(&app),
+	}
+
+	err = srv.ListenAndServe()
+	log.Fatal(err)
+}
---
+++ go.mod
@@ -0,0 +1,9 @@
+module github.com/doh-halle/ntarikoon-park
+
+go 1.17
+
+require (
+	github.com/alexedwards/scs/v2 v2.4.0
+	github.com/go-chi/chi/v5 v5.0.6
+	github.com/justinas/nosurf v1.1.1
+)
